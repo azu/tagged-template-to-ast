bf403b2bd06f8166cdcad64f447ca67c7247b7d5
azu <azuciao@gmail.com>
2016-06-16 01:24:36 +0900

    Breaking Change: use espree

--- src/toAST.js
+++ src/toAST.js
@@ -1,8 +1,38 @@
 // LICENSE : MIT
 "use strict";
-import {parse} from "esprima"
+const espree = require("espree");
 import {generate} from "escodegen"
+const parseOptions = {
+    // attach range information to each node
+    range: true,
+    // attach line/column location information to each node
+    loc: true,
+    // create a top-level comments array containing all comments
+    comment: true,
+    // attach comments to the closest relevant node as leadingComments and
+    // trailingComments
+    attachComment: true,
+    // create a top-level tokens array containing all tokens
+    tokens: true,
+    // specify the language version (3, 5, 6, or 7, default is 5)
+    ecmaVersion: 7,
+    // specify which type of script you're parsing (script or module, default is script)
+    sourceType: "module",
+    // specify additional language features
+    ecmaFeatures: {
+        // enable JSX parsing
+        jsx: true,
+        // enable return in global scope
+        globalReturn: true,
+        // enable implied strict mode (if ecmaVersion >= 5)
+        impliedStrict: true,
+        // allow experimental object rest/spread
+        experimentalObjectRestSpread: true
+    }
+};
+
 function astToCode(expression) {
+
     return generate({
         "type": "Program",
         "body": [
@@ -17,14 +47,10 @@ function astToCode(expression) {
         }
     });
 }
-function extractionBody(ast) {
-    return ast.body[0];
-}
-
 export default function toAST(strings, ...astNodes) {
     var concatCode = strings.map((string, index) => {
         var code = (astNodes[index] ? astToCode(astNodes[index]) : "");
         return string + code;
     }).join("");
-    return parse(concatCode);
+    return espree.parse(concatCode, parseOptions);
 }
